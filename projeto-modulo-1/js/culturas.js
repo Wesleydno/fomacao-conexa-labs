f743bb10dfae58ee471bf1466d9d3203671bdf01
Wesley Reis <wesleydno2011@gmail.com>
2024-04-04 20:31:56 -0300

    criado arquivo de script individual para página

---
+++ projeto-modulo-1/js/culturas.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", function() {
+      const basePath = 'data/culturas.json';
+      const selectCulturas = document.getElementById("culturas");
+      const detalhesCultura = document.getElementById("detalhes-cultura");
+    
+      function carregarCulturas() {
+        fetch(basePath)
+          .then(response => response.json())
+          .then(data => adicionarOpcoesCulturas(data.culturas))
+          .catch(error => console.error("Erro ao carregar o JSON:", error));
+      }
+    
+      function adicionarOpcoesCulturas(culturas) {
+        culturas.forEach(cultura => {
+            const option = document.createElement("option");
+            option.textContent = cultura.nome;
+            option.value = cultura.nome;
+            selectCulturas.appendChild(option);
+          });
+      }
+    
+      function exibirDetalhesCultura(cultura) {
+        detalhesCultura.innerHTML = culturaTemplate(cultura);
+      }
+    
+      function culturaTemplate(cultura) {
+        return `
+        <h2>${cultura.nome}</h2>
+        <p>${cultura.descricao}</p>
+        <p><strong>Principal produtor:</strong> ${cultura.principal_produtor}</p>
+        <p><strong>Produtores:</strong> ${cultura.estados_produtores.join(", ")}</p>
+        <img src="${cultura.link_imagem}" alt="Imagem da cultura ${cultura.nome}">
+      `;
+      }
+    
+      function handleChangeSelect() {
+        const culturaSelecionada = selectCulturas.value;
+        fetch(basePath)
+          .then(response => response.json())
+          .then(data => {
+            const cultura = data.culturas.find(c => c.nome === culturaSelecionada);
+            if (cultura) {
+              exibirDetalhesCultura(cultura);
+            } else {
+              detalhesCultura.innerHTML = "<p>Cultura não encontrada</p>";
+            }
+          })
+          .catch(error => console.error("Erro ao buscar detalhes da cultura:", error));
+      }
+    
+      carregarCulturas();
+    
+      selectCulturas.addEventListener("change", handleChangeSelect);
+    
+    });
