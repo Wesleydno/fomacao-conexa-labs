5530b47282c1f005832b365a1c12a9ff4e8a9d53
Wesley Reis <wesleydno2011@gmail.com>
2024-04-05 16:18:00 -0300

    adicionando opção de templaite das informações inicializanod no construtor

--- projeto-modulo-2/assets/js/culturas.js
+++ projeto-modulo-2/assets/js/culturas.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   class App {
-    constructor(pathData, pathImages, idSelect, idDetalhes) {
+    constructor(pathData, pathImages, idSelect, idDetalhes, templateHtml) {
       this._pathData = pathData;
       this._caminhoBaseImagens = pathImages;
       this._select = document.getElementById(idSelect);
       this._detalhes = document.getElementById(idDetalhes);
       this._dados = [];
+      this._templateHtml = templateHtml;
     }
 
   }
@@ -14,8 +15,15 @@ document.addEventListener("DOMContentLoaded", function () {
   const pathImages = 'assets/images/culturas/'
   const idSelect = "culturas";
   const idDetalhes = "detalhes-cultura";
+
+  const templateHtmlCultura = `
+  <h2>{{nome}}</h2>
+  <p><img src="{{link_imagem}}" width="400px" height="300px" class="alignleft" alt="Imagem da cultura {{nome}}">{{descricao}}</p>
+  <p><strong>Principal produtor:</strong> {{principal_produtor}}</p>
+  <p><strong>Produtores:</strong> {{estados_produtores}}</p>
+  `;
   
-  const appCulturas = new App(pathData, pathImages, idSelect, idDetalhes);
+  const appCulturas = new App(pathData, pathImages, idSelect, idDetalhes, templateHtmlCultura);
   console.log(appCulturas)
 
 });
