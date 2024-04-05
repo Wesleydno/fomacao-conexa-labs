58cb2413ac23d87f95a24eb4bf8fab1eecc41ea7
Wesley Reis <wesleydno2011@gmail.com>
2024-04-05 16:15:47 -0300

    definindo nomes de funções e variaveis mais genéricos

--- projeto-modulo-2/assets/js/culturas.js
+++ projeto-modulo-2/assets/js/culturas.js
@@ -1,32 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
-  class Cultura {
-    constructor(nome, descricao, principalProdutor, estadosProdutores, linkImagem) {
-      this.nome = nome;
-      this.descricao = descricao;
-      this.principalProdutor = principalProdutor;
-      this.estadosProdutores = estadosProdutores;
-      this.linkImagem = linkImagem;
-    }
-  }
-
   class App {
     constructor(pathData, pathImages, idSelect, idDetalhes) {
-      this.pathData = pathData;
-      this.pathImages = pathImages;
-      this.select = document.getElementById(idSelect);
-      this.detalhes = document.getElementById(idDetalhes);
-      this.allData = [];
+      this._pathData = pathData;
+      this._caminhoBaseImagens = pathImages;
+      this._select = document.getElementById(idSelect);
+      this._detalhes = document.getElementById(idDetalhes);
+      this._dados = [];
     }
-  }   
 
+  }
+  
   const pathData = 'data/culturas.json';
-  const pathImages = 'assets/images/culturas/';
+  const pathImages = 'assets/images/culturas/'
   const idSelect = "culturas";
   const idDetalhes = "detalhes-cultura";
-
-  const app = new App(pathData, pathImages, idSelect, idDetalhes);
-
-  console.log(app);
-
+  
+  const appCulturas = new App(pathData, pathImages, idSelect, idDetalhes);
+  console.log(appCulturas)
 
 });
