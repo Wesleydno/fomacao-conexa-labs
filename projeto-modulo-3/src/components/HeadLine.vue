c14df5b67a15705cf96fd271e9698e1f61d5c561
Wesley Reis <wesleydno2011@gmail.com>
2024-04-18 12:02:47 -0300

    atualização componente Headline e routes

--- projeto-modulo-3/src/components/HeadLine.vue
+++ projeto-modulo-3/src/components/HeadLine.vue
@@ -1,5 +1,51 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useRoute } from 'vue-router'
+
+interface IHeadlineItem {
+    title: string
+    description: string
+}
+
+const route = useRoute();
+const headline = computed(() => (route.meta.headline as IHeadlineItem) ?? { title: '', description: '' });
+const isHomePage = computed(() => {
+    return route.name === 'Home';
+})
+
+</script>
+
 <template>
-    <div>
-        <h1>Headline</h1>
+    <div :class="{ 'headline': true, 'principal': isHomePage }">
+        <div class="content-wrapper">
+            <h1>{{ headline.title }}</h1>
+            <h2 v-if="headline.description">{{ headline.description }}</h2>
+        </div>
     </div>
 </template>
+
+<style scoped>
+.headline {
+    background-color: var(--primary-color);
+    color: var(--white-color);
+    padding: 20px 0;
+}
+
+.headline.principal {
+    text-align: center;
+}
+
+.headline h1 {
+    font-size: 1.875rem;
+}
+
+.headline h2 {
+    font-size: 1.063rem;
+}
+
+@media only screen and (max-width: 768px) {
+    .headline {
+        text-align: center;
+    }
+}
+</style>
--- projeto-modulo-3/src/router/index.ts
+++ projeto-modulo-3/src/router/index.ts
@@ -7,21 +7,45 @@ const router = createRouter({
       path: '/',
       name: 'Home',
       component: HomeView,
+      meta: {
+        headline: {
+          title: "Brasil Agrícola",
+          description: "Explorando as Riquezas Agrícolas de Cada Estado Brasileiro",
+        },
+      },
     },
     {
       path: '/produtores',
       name: 'Produtores',
       component: () => import('../views/ProdutoresView.vue'),
+      meta: {
+        headline: {
+          title: "Principais Produtores",
+          description: "",
+        },
+      },
     },
     {
       path: '/culturas',
       name: 'Culturas',
       component: () => import('../views/CulturasView.vue'),
+      meta: {
+        headline: {
+          title: "Principais Culturas",
+          description: "",
+        },
+      },
     },
     {
       path: '/tecnologia',
       name: 'Tecnologia',
       component: () => import('../views/TecnologiaView.vue'),
+      meta: {
+        headline: {
+          title: "Tecnologia",
+          description: "",
+        },
+      },
     }
   ]
 })
