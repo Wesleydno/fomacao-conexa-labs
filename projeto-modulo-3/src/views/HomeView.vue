1adf033269de426f3c1fcca84412805d962ef312
Wesley Reis <wesleydno2011@gmail.com>
2024-04-18 12:15:18 -0300

    pagina home

--- projeto-modulo-3/src/views/HomeView.vue
+++ projeto-modulo-3/src/views/HomeView.vue
@@ -1,5 +1,330 @@
+<script setup lang="ts">
+</script>
+
 <template>
-  <div>
-      <h1>Home</h1>
-  </div>
+  <section>
+    <article>
+      <h2>Um mosaico de diversidade regional</h2>
+      <picture>
+        <img src="@/assets/images/diversidade-no-agro-brasileiro.png" alt="Mapa do brasil por produção agrícola"
+          width="300px" height="300px" class="alignright" />
+      </picture>
+      <p>
+        O Brasil é uma potência agrícola reconhecida mundialmente, e sua produção agrícola é tão vasta
+        quanto
+        a diversidade de seu território. Com peculiaridades climáticas e de solo que se estendem por suas
+        cinco regiões, o país se destaca pela sua capacidade de cultivar uma ampla variedade de culturas.
+        Cada região contribui de forma única para a riqueza e o dinamismo do agronegócio brasileiro. Neste
+        projeto, exploraremos a diversidade da produção agrícola brasileira por região, destacando as
+        culturas predominantes e as peculiaridades de cada estado.
+      </p>
+      <p>Como características do agronegócio, podemos citar as médias e grandes propriedades rurais que
+        conseguem alta produtividade em seus respectivos ramos de produção, tanto na agricultura quanto na
+        pecuária. Na agricultura, é frequente a prática da monocultura, ou seja, a produção de um tipo de
+        plantio apenas. Em muitos casos, há propriedades que praticam as duas modalidades, criando gado e
+        plantando algum tipo de vegetal.</p>
+      <p>As médias e grandes propriedades rurais desempenham um papel importante para outra característica do
+        agronegócio: a modernização do campo. Esta é resultado de intensas transformações científicas e
+        tecnológicas a partir da década de 1950, com maior intensidade na década de 1970, com o surgimento
+        dos Organismos Geneticamente Modificados (OGM), por exemplo.</p>
+      <p>Essa modernização engloba máquinas, sementes modificadas, técnicas de cultivo e criação de animais
+        avançadas e desenvolvimento de produtos que aceleram o ciclo natural de plantio e crescimento de
+        plantas e gados. Existem laboratórios especializados em aprimorar espécies de animais ou para
+        cultivos, a fim de melhorar e aumentar a produção.</p>
+    </article>
+    <article>
+      <h3>Produção agrícola por estado no Brasil</h3>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Estado</th>
+            <th>Principal Cultura</th>
+            <th>Área Cultivada (ha)</th>
+            <th>Produção Total (ton)</th>
+            <th>Valor da Produção Agrícola (milhões de R$)</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Acre</td>
+            <td>Pecuária</td>
+            <td>500.000</td>
+            <td>200.000</td>
+            <td>300</td>
+          </tr>
+          <tr>
+            <td>Alagoas</td>
+            <td>Cana-de-açúcar</td>
+            <td>1.000.000</td>
+            <td>10.000.000</td>
+            <td>1.500</td>
+          </tr>
+          <tr>
+            <td>Amapá</td>
+            <td>Mandioca</td>
+            <td>300.000</td>
+            <td>500.000</td>
+            <td>150</td>
+          </tr>
+          <tr>
+            <td>Amazonas</td>
+            <td>Pecuária</td>
+            <td>1.500.000</td>
+            <td>800.000</td>
+            <td>400</td>
+          </tr>
+          <tr>
+            <td>Bahia</td>
+            <td>Soja</td>
+            <td>5.000.000</td>
+            <td>10.000.000</td>
+            <td>7.000</td>
+          </tr>
+          <tr>
+            <td>Ceará</td>
+            <td>Milho</td>
+            <td>800.000</td>
+            <td>2.000.000</td>
+            <td>1.200</td>
+          </tr>
+          <tr>
+            <td>Distrito Federal</td>
+            <td>Grãos e Hortaliças</td>
+            <td>200.000</td>
+            <td>1.500.000</td>
+            <td>1.000</td>
+          </tr>
+          <tr>
+            <td>Espírito Santo</td>
+            <td>Café</td>
+            <td>1.200.000</td>
+            <td>2.500.000</td>
+            <td>3.000</td>
+          </tr>
+          <tr>
+            <td>Goiás</td>
+            <td>Soja</td>
+            <td>8.000.000</td>
+            <td>15.000.000</td>
+            <td>10.000</td>
+          </tr>
+          <tr>
+            <td>Maranhão</td>
+            <td>Arroz</td>
+            <td>3.000.000</td>
+            <td>5.000.000</td>
+            <td>2.500</td>
+          </tr>
+          <tr>
+            <td>Mato Grosso</td>
+            <td>Soja</td>
+            <td>9.000.000</td>
+            <td>30.000.000</td>
+            <td>20.000</td>
+          </tr>
+          <tr>
+            <td>Mato Grosso do Sul</td>
+            <td>Cana-de-açúcar</td>
+            <td>3.500.000</td>
+            <td>20.000.000</td>
+            <td>12.000</td>
+          </tr>
+          <tr>
+            <td>Minas Gerais</td>
+            <td>Café</td>
+            <td>2.500.000</td>
+            <td>6.000.000</td>
+            <td>5.000</td>
+          </tr>
+          <tr>
+            <td>Pará</td>
+            <td>Soja</td>
+            <td>4.500.000</td>
+            <td>12.000.000</td>
+            <td>8.000</td>
+          </tr>
+          <tr>
+            <td>Paraíba</td>
+            <td>Cana-de-açúcar</td>
+            <td>700.000</td>
+            <td>5.000.000</td>
+            <td>1.000</td>
+          </tr>
+          <tr>
+            <td>Paraná</td>
+            <td>Soja</td>
+            <td>6.500.000</td>
+            <td>18.000.000</td>
+            <td>15.000</td>
+          </tr>
+          <tr>
+            <td>Pernambuco</td>
+            <td>Cana-de-açúcar</td>
+            <td>1.200.000</td>
+            <td>8.000.000</td>
+            <td>3.500</td>
+          </tr>
+          <tr>
+            <td>Piauí</td>
+            <td>Soja</td>
+            <td>2.000.000</td>
+            <td>6.000.000</td>
+            <td>4.000</td>
+          </tr>
+          <tr>
+            <td>Rio de Janeiro</td>
+            <td>Café</td>
+            <td>300.000</td>
+            <td>500.000</td>
+            <td>600</td>
+          </tr>
+          <tr>
+            <td>Rio Grande do Norte</td>
+            <td>Fruticultura</td>
+            <td>400.000</td>
+            <td>2.000.000</td>
+            <td>700</td>
+          </tr>
+          <tr>
+            <td>Rio Grande do Sul</td>
+            <td>Soja</td>
+            <td>5.000.000</td>
+            <td>14.000.000</td>
+            <td>13.000</td>
+          </tr>
+          <tr>
+            <td>Rondônia</td>
+            <td>Pecuária</td>
+            <td>1.200.000</td>
+            <td>700.000</td>
+            <td>300</td>
+          </tr>
+          <tr>
+            <td>Roraima</td>
+            <td>Pecuária</td>
+            <td>800.000</td>
+            <td>400.000</td>
+            <td>200</td>
+          </tr>
+          <tr>
+            <td>Santa Catarina</td>
+            <td>Avicultura</td>
+            <td>700.000</td>
+            <td>2.000.000</td>
+            <td>2.500</td>
+          </tr>
+          <tr>
+            <td>São Paulo</td>
+            <td>Cana-de-açúcar</td>
+            <td>4.000.000</td>
+            <td>25.000.000</td>
+            <td>18.000</td>
+          </tr>
+          <tr>
+            <td>Sergipe</td>
+            <td>Cana-de-açúcar</td>
+            <td>500.000</td>
+            <td>3.000.000</td>
+            <td>800</td>
+          </tr>
+          <tr>
+            <td>Tocantins</td>
+            <td>Soja</td>
+            <td>2.500.000</td>
+            <td>7.000.000</td>
+            <td>5.000</td>
+          </tr>
+        </tbody>
+      </table>
+    </article>
+  </section>
 </template>
+
+<style>
+.table {
+  margin-top: 20px;
+  border-collapse: collapse;
+  border-spacing: 0;
+  width: 100%;
+  border: 1px solid var(--table-border-color);
+}
+
+td:first-child {
+  color: var(--primary-color);
+  font-weight: 600;
+  text-decoration: none;
+}
+
+th,
+td {
+  text-align: left;
+  padding: 16px;
+  border: 1px solid var(--table-border-color);
+  text-align: center;
+}
+
+tr:nth-child(even) {
+  background-color: var(--table-background-color);
+}
+
+@media screen and (max-width: 600px) {
+  .table {
+    border: none;
+  }
+
+  table,
+  thead,
+  tbody,
+  th,
+  td,
+  tr {
+    display: block;
+  }
+
+  thead {
+    display: none;
+  }
+
+  tbody tr {
+    margin-bottom: 20px;
+    border: 1px solid var(--table-border-color);
+  }
+
+  td {
+    border: none;
+    border-bottom: 1px solid var(--table-background-color);
+    position: relative;
+    padding-left: 50%;
+    text-align: right;
+  }
+
+  td:before {
+    position: absolute;
+    text-align: left;
+    left: 6px;
+    width: 45%;
+    padding-right: 10px;
+    white-space: nowrap;
+  }
+
+  td:nth-of-type(1):before {
+    content: "Estado";
+  }
+
+  td:nth-of-type(2):before {
+    content: "Principal Cultura";
+  }
+
+  td:nth-of-type(3):before {
+    content: "Área Cultivada (ha)";
+  }
+
+  td:nth-of-type(4):before {
+    content: "Produção Total (ton)";
+  }
+
+  td:nth-of-type(5):before {
+    content: "VL Produção (milhões de R$)";
+  }
+}
+</style>
