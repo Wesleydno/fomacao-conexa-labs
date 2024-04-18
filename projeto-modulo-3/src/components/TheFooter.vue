00006d60c41eec3038077147804fdb75eca27286
Wesley Reis <wesleydno2011@gmail.com>
2024-04-18 12:04:13 -0300

    atualização componente footer

--- projeto-modulo-3/src/components/TheFooter.vue
+++ projeto-modulo-3/src/components/TheFooter.vue
@@ -1,5 +1,22 @@
+<script setup lang="ts">
+</script>
+
 <template>
-  <div>
-      <h1>Footer</h1>
-  </div>
+  <footer>
+    <p>© 2024 by Wesley Reis | <a href="mailto:email@example.com">wesley.souza@noufiscal.com.br</a></p>
+  </footer>
 </template>
+
+<style scoped>
+footer {
+  text-align: center;
+  padding: 20px;
+  background-color: var(--primary-color);
+}
+
+footer p,
+a {
+  color: var(--white-color);
+  margin: 0;
+}
+</style>
